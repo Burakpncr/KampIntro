2039f0ae83f5cceed4826c556299dbcf59db7c87
Burak Pancar <83040277+Burakpncr@users.noreply.github.com>
2021-04-23 03:25:50 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] kurslar = new string[] { "Yazılım Geliştime Kampı", "Programlamaya Giriş için Temel Kurs","Java","python","C#" };
+           
+            Console.WriteLine("sayfa başı - header");
+            
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+            
+            Console.WriteLine("For bitti");
+            
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("sayfa sonu - footer");
+        }
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            double dolarDun = 7.45;
+            double dolarBugun = 7.45;
+            bool sistemeGirisYapmisMi = true;
+            
+            if (dolarDun > dolarBugun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else if(dolarBugun > dolarDun)
+            {
+                Console.WriteLine("Artış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Eşittir butonu");
+            }
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı Ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş Yap Butonu");
+            }
+
+            Console.WriteLine(kategoriEtiketi);
+           
+        }
+    }
+}
